53ea41a70b95545b6c7a57846431bb98b5ea00a9
SwatColombia <fabetancur10@misena.edu.co>
2024-11-22 14:16:21 -0500

    resolve problems

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,12 +1,29 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import usuarioRoutes from './routes/usuarioRoutes.js'
+import connectDB from './database/db.js';
 
-
-//import connectDB from './database/db.js';
-
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 4000;
 
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.DB_MONGO, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Conectado a la base de datos MongoDB');
+    } catch (err) {
+        console.error('Error conectándose a la base de datos MongoDB:', err);
+        process.exit(1);
+    }
+};
+connectDB();
+
+app.use(express.json());
 
 app.get('/', usuarioRoutes)
 
@@ -31,7 +48,6 @@ catch(err => console.error('Error al conectarse a MongoDB:', err));
 //connectDB();
 // routing 
 
-const port = 4000;
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`
 
