bf6939072bcda827100b7900ee3e95d6eaf5d2e3
SwatColombia <fabetancur10@misena.edu.co>
2024-11-22 08:45:34 -0500

    update node-modules

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
+const connectDB = require('./database/db');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/miBaseDeDatos',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(() =>
-        console.log('Conectado a MongoDB')).
-    catch(err => console.error('Error al conectarse a MongoDB:', err));
+mongoose.connect('connectDB', {
 
+}).then(() =>
+    console.log('Conectado a MongoDB')).
+catch(err => console.error('Error al conectarse a MongoDB:', err));
+
+connectDB();
 
 app.get('/', (req, res) => { res.send('¡Hola, mundo!'); });
 
