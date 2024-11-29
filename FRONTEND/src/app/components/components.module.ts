b7535c0e559c16694d95baefd593eeafdb8ff192
SwatColombia <fabetancur10@misena.edu.co>
2024-11-29 09:49:20 -0500

    subiendo cambios

--- FRONTEND/src/app/components/components.module.ts
+++ FRONTEND/src/app/components/components.module.ts
@@ -7,11 +7,12 @@ import { HomeComponent } from './home/home.component';
 import { WorksComponent } from './works/works.component';
 import { ArtistsService } from '../services/artists.service';
 import { CarritoComponent } from './carrito/carrito.component';
+import { UploadComponent } from './upload/upload.component';
 
 
 @NgModule({
   exports: [ ArtistsComponent,HomeComponent,WorksComponent],
-  declarations: [ArtistsComponent, HomeComponent,WorksComponent, CarritoComponent],
+  declarations: [ArtistsComponent, HomeComponent,WorksComponent, CarritoComponent, UploadComponent],
   imports: [CommonModule],
   providers: [ArtistsService],
 })
